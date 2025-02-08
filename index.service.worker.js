fc4801ef63935424f097e14c9235178d1e954fa9
Álvaro E. Martos Suero <49306506+alvez29@users.noreply.github.com>
2025-02-08 23:08:04 +0100

    Add files via upload

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1739052044|42774266034';
+const CACHE_VERSION = '1739052465|43194674986';
 /** @type {string} */
 const CACHE_PREFIX = 'portfolio-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
